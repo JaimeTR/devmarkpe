ce8271fcf577607a3adb55173d5ff87bcd4837ee
Jaime Tarazona ✅ <38092883+JaimeTR@users.noreply.github.com>
2024-05-18 05:02:33 -0500

    feat: update editor settings and metadata
    The editor settings have been updated to enable word wrap and add support for English and Spanish spell checking. Additionally, the metadata for the application has been modified to reflect the new name and description of "Devmark Studio" and "Agencia de desarrollo web y marketing digital" respectively.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Devmark Studio",
+  description: "Agencia de desarrollo web y marketing digital",
 };
 
 export default function RootLayout({
